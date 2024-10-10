2c9065efa09b09e811fcf85d520a26122f8fdff9
Vincent Kipkoech <Mail.Vincentk@gmail.com>
2024-10-10 02:46:51 -0500

    Create and Render a Form - Video

--- src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingeventsApplication {
+public class CodingEventsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodingeventsApplication.class, args);
+		SpringApplication.run(CodingEventsApplication.class, args);
 	}
 
 }
